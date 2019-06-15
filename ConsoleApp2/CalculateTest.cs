7cc2c554d8661574388b4a02b22aee46cd07f1de
minaammunir <minaammunir@gmail.com>
2019-06-15 21:03:19 +0500

    Initial Commit

---
+++ ConsoleApp2/CalculateTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    [TestFixture]
+    class CalculateTest
+    {
+        [TestCase(1, 2, 3)]
+        [TestCase(4, 2, 6)]
+        [TestCase(10, 2, 12)]
+        public void Sum_ParametersGiven_CheckBehaviour(int a, int b, int expectedResult)
+        {
+            // Arrange
+            var testEntity = new Calculate();
+
+            // Act
+            var result = testEntity.Sum(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
